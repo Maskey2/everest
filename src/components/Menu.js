93fb4abfe28753a7de07cccf5aa5d34767e30bc2
Maskey2 <maskeynishma@gmail.com>
2022-04-19 17:00:27 -0500

    menu added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,6 +3,7 @@ import { Container, Grid } from '@material-ui/core';
 import Img from '../images/Everest_Menu_1.jpg'
 import Img2 from '../images/Everest_Menu_2.jpg'
 import {  Typography } from "@material-ui/core";
+import Menu from './../menu/Everest_Menu.pdf'
 
 export default function Users() {
     return (
@@ -10,11 +11,11 @@ export default function Users() {
             <h1 style={{textAlign:'center', backgroundColor:'#e4e4e4', padding:'10px',marginTop:'70px'}}>MENU</h1>
             <Grid container spacing={2} justify="center" alignItems="center">                    
           <Grid item  xs={12} sm={12} md={12}>
-              <a href="https://everest.onrender.com/menu/Everest-Menu.pdf">
+              <a href={Menu}>
           <img src={Img} alt="Menu" style={{maxWidth:'100%',height:'auto'}}/></a>
               </Grid>  
               <Grid item  xs={12} sm={12} md={12}>
-              <a href="https://everest.onrender.com/menu/Everest-Menu.pdf">
+              <a href={Menu}>
           <img src={Img2} alt="Menu" style={{maxWidth:'100%',height:'auto'}}/></a>
               </Grid> 
               
